6abada396d5eb83dfba04a4e95e968f141d16892
nick <nicholasxjy@gmail.com>
2026-05-15 20:20:40 +0800

    Initial Commit

---
+++ yabai-space-marker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  yabai-space-marker
+//
+//  Created by 薛晶义 on 2026/5/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ yabai-space-marker/yabai_space_markerApp.swift
@@ -0,0 +1,17 @@
+//
+//  yabai_space_markerApp.swift
+//  yabai-space-marker
+//
+//  Created by 薛晶义 on 2026/5/15.
+//
+
+import SwiftUI
+
+@main
+struct yabai_space_markerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
